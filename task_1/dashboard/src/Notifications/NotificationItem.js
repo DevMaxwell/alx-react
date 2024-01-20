cd61335850a4425969110ab7781ede412f195500
DevMaxwell <89994817+DevMaxwell@users.noreply.github.com>
2024-01-19 18:01:02 -0800

    task: 0x04. React inline styling

--- task_1/dashboard/src/Notifications/NotificationItem.js
+++ task_1/dashboard/src/Notifications/NotificationItem.js
@@ -1,26 +1,55 @@
-import React from "react";
-import "./Notifications.css";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
-function NotificationItem({ type, html, value }) {
-  return (
-    <>
-      {type && value ? <li data-notification-type={type}>{value}</li> : null}
-      {html ? <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
-    </>
-  );
+const styles = StyleSheet.create({
+	default: {
+		color: 'blue',
+	},
+	urgent: {
+		color: 'red',
+	},
+});
+
+class NotificationItem extends React.PureComponent {
+	render() {
+		const { type, value, html, markAsRead, id } = this.props;
+		return (
+			<React.Fragment>
+				{type && value ? (
+					<li
+						className={
+							type === 'default' ? css(styles.default) : css(styles.urgent)
+						}
+						onClick={() => markAsRead(id)}
+						data-notification-type={type}
+					>
+						{value}
+					</li>
+				) : null}
+				{html ? (
+					<li
+						onClick={() => markAsRead(id)}
+						data-urgent
+						className={css(styles.urgent)}
+						dangerouslySetInnerHTML={{ __html: html }}
+					></li>
+				) : null}
+			</React.Fragment>
+		);
+	}
 }
 
 NotificationItem.propTypes = {
-  type: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  __html: PropTypes.shape({
-    html: PropTypes.string,
-  }),
+	type: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	__html: PropTypes.shape({
+		html: PropTypes.string,
+	}),
 };
 
 NotificationItem.defaultProps = {
-  type: "default",
+	type: 'default',
 };
 
 export default NotificationItem;
